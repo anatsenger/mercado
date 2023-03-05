38282494acf0dfa4469b658d8c8d532104eb03d4
Mercia <68623425+mercietc@users.noreply.github.com>
2023-03-05 14:39:25 -0300

    upload Feature/receive messages rabbit mq - financeiro

--- src/main/java/com/example/mercado/MercadoApplication.java
+++ src/main/java/com/example/mercado/MercadoApplication.java
@@ -1,8 +1,15 @@
 package com.example.mercado;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@OpenAPIDefinition(info = @Info(title = "Sample api", version = "1.0", description = "Sample API"))
+@EnableCaching
+@EnableRabbit
 @SpringBootApplication
 public class MercadoApplication {
 
